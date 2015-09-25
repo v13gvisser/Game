55142cad028c01cd3744dfd7b105035b204ee5d2
Glenn Visser <131541@helenparkhurst.net>
2015-09-25 23:58:01 +0200

    uitleg in "uitleg jakkiebak Kippekak in src.

---
+++ src/glennDbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$dbh = new PDO('mysql:host=v13gvisser.helenparkhurst.net;dbname=Game', 'v13gvisser', '3tCHz7qP');
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ src/glennTests/glennServerTest.php
@@ -0,0 +1,11 @@
+<?php
+require_once('glennServer.php');
+
+class glennServerTest extends PHPUnit_Framework_TestCase {
+    public function testOperational() {
+        $this->assertTrue(true);
+    }
+    
+    
+}
+
